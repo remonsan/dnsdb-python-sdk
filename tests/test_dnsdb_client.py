8272bdd92d1730c391d254c0da6fc6e5921603a7
fengyouchao <fengyouchao@gmail.com>
2016-09-21 15:40:31 +0800

    add somes tests

--- tests/test_dnsdb_client.py
+++ tests/test_dnsdb_client.py
@@ -1,12 +1,14 @@
 from __future__ import print_function, unicode_literals
 from dnsdb.api import APIClient
 from dnsdb.clients import DnsDBClient
-from dnsdb.errors import AuthenticationError, DnsDBException
+from dnsdb.errors import AuthenticationError, DnsDBException, APIServerError
 from nose import with_setup
-from nose.tools import raises, assert_equal
+from nose.tools import raises, assert_equal, assert_false, assert_true
 from dnsdb_mock_apiserver import mockserver
 from dnsdb_mock_apiserver.models import User
-import time
+from dnsdb_mock_apiserver.errors import InternalServerError, GatewayTimeoutError
+import datetime
+import json
 
 default_username = 'test'
 default_password = '123456'
@@ -15,6 +17,7 @@ default_remaining_request = 100
 
 def setup_func():
     APIClient.API_BASE_URL = 'http://localhost:8080/api/v1'
+    mockserver.return_error_response = None
     mockserver.start()
     mockserver.users.append(User(default_username, default_password, default_remaining_request))
 
@@ -24,10 +27,13 @@ def teardown_func():
 
 
 @with_setup(setup_func, teardown_func)
-def test_login_success():
+def test_login():
     mockserver.default_access_token = 'dnsdb api access-token'
     client = DnsDBClient()
+    assert_false(client.is_login())
     client.login(default_username, default_password)
+    assert_true(client.is_login())
+    assert_equal(json.loads(str(client.access_token))['access_token'], mockserver.default_access_token)
     assert_equal(client.access_token.token, mockserver.default_access_token)
 
 
@@ -39,7 +45,7 @@ def test_login_failed():
 
 
 @with_setup(setup_func, teardown_func)
-def test_search_dns_success():
+def test_search_dns():
     user = User(default_username, default_password, 2)
     mockserver.users = [user]
     mockserver.dns_records = [
@@ -47,12 +53,20 @@ def test_search_dns_success():
                              ] * 100
     client = DnsDBClient()
     client.login(default_username, default_password)
-    results = client.search_dns(domain='a.com')
+    results = client.search_dns(domain='a.com', ip='1.1.0.0', dns_type='a', host='c.a.com')
     assert_equal(len(results), 30)
     assert_equal(user.remaining_request, 1)
     results = client.search_dns(domain='a.com', start=99)
     assert_equal(len(results), 1)
     assert_equal(user.remaining_request, 0)
+    for record in results:
+        assert_equal(record['host'], record.host)
+        assert_equal(record['type'], record.type)
+        assert_equal(record['value'], record.value)
+        data = json.loads(str(record))
+        assert_equal(data['host'], record.host)
+        assert_equal(data['type'], record.type)
+        assert_equal(data['value'], record.value)
 
 
 @raises(AuthenticationError)
@@ -79,6 +93,24 @@ def test_search_dns_without_remaining_request():
     client.search_dns(domain='a.com')
 
 
+@raises(APIServerError)
+@with_setup(setup_func, teardown_func)
+def test_search_dns_return_500():
+    client = DnsDBClient()
+    client.login(default_username, default_password)
+    mockserver.return_error_response = InternalServerError
+    client.search_dns(domain='a.com')
+
+
+@raises(APIServerError)
+@with_setup(setup_func, teardown_func)
+def test_search_dns_return_504():
+    client = DnsDBClient()
+    client.login(default_username, default_password)
+    mockserver.return_error_response = GatewayTimeoutError
+    client.search_dns(domain='a.com')
+
+
 @with_setup(setup_func, teardown_func)
 def test_retrieve_dns():
     total = 200
@@ -89,7 +121,9 @@ def test_retrieve_dns():
                              ] * total
     client = DnsDBClient()
     client.login(default_username, default_password)
-    results = client.retrieve_dns(domain='a.com')
+    client.access_token.expire_at = datetime.datetime.now()
+    assert_true(client.access_token.has_expired())
+    results = client.retrieve_dns(domain='a.com', dns_type='a', ip='1.1.1.1', host='c.a.com')
     assert_equal(len(results), total)
     count = 0
     for _ in results:
@@ -97,11 +131,13 @@ def test_retrieve_dns():
     assert_equal(count, total)
 
 
-@raises(AuthenticationError)
 @with_setup(setup_func, teardown_func)
 def test_retrieve_dns_without_login():
     client = DnsDBClient()
-    client.retrieve_dns(domain='a.com')
+    try:
+        client.retrieve_dns(domain='a.com')
+    except DnsDBException as e:
+        assert_equal(str(e), 'Require login')
 
 
 @raises(DnsDBException)
